3e7fafc253b4509922c513d0044c1791632e3a1e
Casey Hald <hald.casey@gmail.com>
2025-05-11 11:31:19 -0700

    init

--- code.js
+++ code.js
@@ -1,1 +1,44 @@
-throw new Error("This plugin template uses TypeScript. Follow the instructions in `README.md` to generate `code.js`.")
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+// import figma types
+var VariableResolvedDataEnum;
+(function (VariableResolvedDataEnum) {
+    VariableResolvedDataEnum["STRING"] = "STRING";
+    VariableResolvedDataEnum["COLOR"] = "COLOR";
+    VariableResolvedDataEnum["BOOLEAN"] = "BOOLEAN";
+    VariableResolvedDataEnum["FLOAT"] = "FLOAT";
+})(VariableResolvedDataEnum || (VariableResolvedDataEnum = {}));
+function rgbToHex({ r, g, b, a } = { r: 0, g: 0, b: 0, a: 1 }) {
+    const toHex = (value) => {
+        const hex = Math.round(value * 255).toString(16);
+        return hex.length === 1 ? "0" + hex : hex;
+    };
+    const hex = [toHex(r), toHex(g), toHex(b)];
+    if (a !== 1) {
+        hex.push(toHex(a));
+    }
+    return `#${hex.join("")}`;
+}
+const getLocalVariableCollections = () => __awaiter(void 0, void 0, void 0, function* () {
+    const variableJSON = {};
+    const colorVariables = yield figma.variables.getLocalVariablesAsync(VariableResolvedDataEnum.COLOR);
+    yield Promise.all(colorVariables.map((colorVariable) => __awaiter(void 0, void 0, void 0, function* () {
+        const { name, valuesByMode } = yield figma.variables.getVariableByIdAsync(colorVariable.id);
+        const modeIds = Object.keys(valuesByMode);
+        const value = valuesByMode[modeIds[0]];
+        if (typeof value.r === "number") {
+            variableJSON[name] = rgbToHex({ r: value.r, g: value.g, b: value.b, a: value.a });
+        }
+    })));
+    console.log(variableJSON);
+});
+getLocalVariableCollections();
+//  figma.closePlugin();
--- code.ts
+++ code.ts
@@ -1,22 +1,44 @@
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
-
-// This plugin creates rectangles on the screen.
-const numberOfRectangles = 5;
-
-const nodes: SceneNode[] = [];
-for (let i = 0; i < numberOfRectangles; i++) {
-  const rect = figma.createRectangle();
-  rect.x = i * 150;
-  rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-  figma.currentPage.appendChild(rect);
-  nodes.push(rect);
+// import figma types
+enum VariableResolvedDataEnum {
+  STRING = 'STRING',
+  COLOR = 'COLOR',
+  BOOLEAN = 'BOOLEAN',
+  FLOAT = 'FLOAT',
 }
-figma.currentPage.selection = nodes;
-figma.viewport.scrollAndZoomIntoView(nodes);
 
-// Make sure to close the plugin when you're done. Otherwise the plugin will
-// keep running, which shows the cancel button at the bottom of the screen.
+function rgbToHex({ r, g, b, a } = { r: 0, g: 0, b: 0, a: 1 }) {
+  const toHex = (value: number) => {
+    const hex = Math.round(value * 255).toString(16);
+    return hex.length === 1 ? "0" + hex : hex;
+  };
+
+  const hex = [toHex(r), toHex(g), toHex(b)];
+  if (a !== 1) {
+    hex.push(toHex(a));
+  }
+  return `#${hex.join("")}`;
+}
+
+const getLocalVariableCollections = async () => {
+    const variableJSON = {} as Record<string, string>;
+
+    const colorVariables = await figma.variables.getLocalVariablesAsync(VariableResolvedDataEnum.COLOR);
+
+    await Promise.all(
+      colorVariables.map(async (colorVariable) => {
+        const { name, valuesByMode } = await figma.variables.getVariableByIdAsync(colorVariable.id) as any;
+        const modeIds = Object.keys(valuesByMode);
+        const value = valuesByMode[modeIds[0]];
+        if (typeof value.r === "number") {
+          variableJSON[name] = rgbToHex({ r: value.r, g: value.g, b: value.b, a: value.a });
+        }
+      })
+    );
+
+    console.log(variableJSON);
+}
+
+getLocalVariableCollections();
+
 figma.closePlugin();
+
